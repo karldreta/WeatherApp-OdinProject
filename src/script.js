e98706a84bc4497633c3615ae5a85b228de6bd11
Karl James <karldreta@gmail.com>
2024-09-15 05:48:38 +0800

    Add dynamic update of buttons' textContent

--- src/script.js
+++ src/script.js
@@ -1,11 +1,34 @@
 import "./styles.css";
+import { format } from "date-fns";
 import { fetchLocation } from "./modules/handleAPI";
 
-document.querySelector("form").addEventListener("submit", displayToDOM);
+document.addEventListener("DOMContentLoaded", e => {
+    document.querySelector("form").addEventListener("submit", displayToDOM);
+    async function displayToDOM(e) {
+        const locationDetails = await fetchLocation(e);
+        console.log(locationDetails);
+        console.log(locationDetails.currentWeek[0].temp);
+    
+        const daysOfWeek = convertDateToName(locationDetails.currentWeek);
+        console.log(daysOfWeek);
+        const buttons = document.querySelectorAll(".button");
+        buttons.forEach((button, index) => {
+            if (index <= daysOfWeek.length) { 
+                button.textContent = daysOfWeek[index];
+            }
+        });
+        
+    }
+    
+    function convertDateToName(dates) {
+        const nameOfDays = [];
+        dates.forEach(day => {
+            nameOfDays.push(format(day.datetime, "E"));
+        });
+        return nameOfDays;
+    }
+})
 
 
-async function displayToDOM(e) {
-    const locationDetails = await fetchLocation(e);
-    console.log(locationDetails);
-    console.log(locationDetails.currentWeek[0].temp);
-}
+
+// 0 : {datetime: '2024-09-15', datetimeEpoch: 1726329600, tempmax: 85.7, tempmin: 76.2, temp: 80.5, …}
