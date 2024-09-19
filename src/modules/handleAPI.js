2cf0bb22768a4fac1ad685b52329c937fccbdf22
Karl James <karldreta@gmail.com>
2024-09-19 10:20:54 +0800

    Add error handling

--- src/modules/handleAPI.js
+++ src/modules/handleAPI.js
@@ -2,17 +2,25 @@ export async function fetchLocation(e, dateToday, dateEnd) {
   e.preventDefault();
 
   const locationValue = document.querySelector("#inputLocation").value;
-  const fetchWeatherReport = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationValue}/${dateToday}/${dateEnd}?key=3BZCY9DLAUZ38XRC8GGN95VYW`,
-  );
-  const weatherReport = await fetchWeatherReport.json();
 
-  // Isolate the values we need
-  const currentWeek = weatherReport.days;
-  const timezone = weatherReport.timezone;
-  const address = weatherReport.resolvedAddress;
-  const descriptionWeek = weatherReport.description;
-  const tempWeek = Math.round(weatherReport.currentConditions.temp);
+  try {
+    const fetchWeatherReport = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationValue}/${dateToday}/${dateEnd}?key=3BZCY9DLAUZ38XRC8GGN95VYW`,
+    );
+    const weatherReport = await fetchWeatherReport.json();
+    // Isolate the values we need
+    const currentWeek = weatherReport.days;
+    const timezone = weatherReport.timezone;
+    const address = weatherReport.resolvedAddress;
+    const descriptionWeek = weatherReport.description;
+    const tempWeek = Math.round(weatherReport.currentConditions.temp);
 
-  return { currentWeek, descriptionWeek, tempWeek, timezone, address };
+    return { currentWeek, descriptionWeek, tempWeek, timezone, address };
+  } catch (error) {
+    const alertModal = document.querySelector("dialog.alert");
+    alertModal.showModal();
+    document.querySelector(".closeModal").addEventListener("click", () => {
+      alertModal.close();
+    });
+  }
 }
